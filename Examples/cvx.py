8fa40fbc28fbcd850c619f515064cf28eb61fc66
Yuyang Shu <yuyang.shu@gmail.com>
2018-10-07 13:28:57 +1100

    add basic testing

--- Examples/cvx.py
+++ Examples/cvx.py
@@ -1,6 +1,5 @@
 import os.path
 import csv
-import math
 import heapq
 import pickle
 import collections
@@ -99,15 +98,30 @@ def lr_solve(X_train, X_test, Y_train, Y_test):
 
     model = linear_model.LinearRegression(n_jobs = -1)
     model.fit(X_train, Y_train)
-    print(model.coef_)
+    # print(model.coef_)
     Y_pred = model.predict(X_test)
-    print("RMSE:", math.sqrt(metrics.mean_squared_error(Y_pred, Y_test)))
+    print("RMSE:", numpy.sqrt(metrics.mean_squared_error(Y_pred, Y_test)))
     print("r2_score:", metrics.r2_score(Y_pred, Y_test))
 
-def cvx_solve(X_train, X_test, Y_train, Y_test, lamb = 1, mu = 0.1):
+def cvx_solve(X_train, X_test, Y_train, Y_test, lamb = 1, mu = 0.1, num_neighbours = 3):
 
-    def distance(i, j):
-        return cvxpy.norm(locations[i] - locations[j], 2).value
+    def distance(a, b):
+        return cvxpy.norm(a - b, 2).value
+    
+    def predict(item_location, item_X, *neighbours):
+        total_weights = 0
+        prediction_acc = 0
+        for neighbour in neighbours:
+            dist, j = neighbour
+            if dist > 0:
+                # property does not fall on any of the models
+                total_weights += 1 / dist
+                prediction_acc += (S[j] * item_X / dist).value
+            else:
+                # if the property falls right on one model
+                return (S[j] * item_X).value
+        
+        return prediction_acc / total_weights
 
     # setup
     n = len(X_train)
@@ -134,16 +148,15 @@ def cvx_solve(X_train, X_test, Y_train, Y_test, lamb = 1, mu = 0.1):
     for i in range(n):
         target += (S[i] * X_train[i] - Y_train[i]) ** 2
 
-    # add 3 edges for each node, to nearest neighbours
+    # add num_neighbours edges for each node, to nearest neighbours
     for i in range(n):
         edge_pool = []
         for j in range(n):
             if i != j:
-                heapq.heappush(edge_pool, (distance(i, j), i, j))
-        for _ in range(3):
+                heapq.heappush(edge_pool, (distance(locations[i], locations[j]), i, j))
+        for _ in range(num_neighbours):
             edge_to_add = heapq.heappop(edge_pool)
-            dst = edge_to_add[0]
-            j = edge_to_add[2]
+            dst, j = edge_to_add[0], edge_to_add[2]
             if j not in connections[i]:
                 connections[i] += [(j, dst)]
                 connections[j] += [(i, dst)]
@@ -151,19 +164,39 @@ def cvx_solve(X_train, X_test, Y_train, Y_test, lamb = 1, mu = 0.1):
     for i in range(n):
         for j, dst in connections[i]:
             if dst > 0:
-                # lasso
+                # lasso term
                 target += lamb / dst * cvxpy.norm(S[i] - S[j], 2)
             else:
-                # same location => add constraint
+                # same location => constrain to be the same model
                 constraints.append(S[i] == S[j])
 
+    # learn the models
     problem = cvxpy.Problem(cvxpy.Minimize(target), constraints)
     result = problem.solve()
-    print(result)
-    for item in S:
-        print(item.value)
 
-    # test the model
+    # print total training error and model parameters
+    print("approximate training RMSE:", numpy.sqrt(result / n))
+    # for item in S:
+        # print(item.value)
+
+    # test on dev set
+    predictions = []
+    for item in X_test:
+        item_location = numpy.array((item[5], item[6]))
+        item_X = numpy.array((item[:3] + [1]))
+
+        # find the nearest models and predict based on them
+        neighbour_heap = []
+        for i in range(n):
+            heapq.heappush(neighbour_heap, (distance(locations[i], item_location), i))
+        neighbours = []
+        for i in range(num_neighbours):
+            neighbours.append(heapq.heappop(neighbour_heap))
+        predictions.append(predict(item_location, item_X, *neighbours))
+
+    # sum of squared error
+    # print(numpy.array(predictions) - numpy.array(Y_test))
+    print("RMSE:", numpy.sqrt(metrics.mean_squared_error(predictions, Y_test)))
 
 
 #__main__
@@ -182,18 +215,18 @@ Y = [item[-1] for item in selected_data]
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 ## feed into model ##
-# lr_solve(X_train, X_test, Y_train, Y_test)
+lr_solve(X_train, X_test, Y_train, Y_test)
 
 # if run regression with 7 features
 # RMSE: 82025.51851690875
 # r2_score: -3.961004990007666
-# [-1.30471340e+01 -3.22226437e+03 -1.47154757e+04  2.26577883e+04
-#  -3.09251108e+01 -2.67852669e+06  5.54130897e+06  0.00000000e+00]
 
 # run regression with 3 features
 # RMSE: 91193.75444985367
 # r2_score: -13.579062442242591
-# [  -14.90423172 -4560.70742336  7427.1270868      0.        ]
 
-# run snapvx
+# run cvxpy solver
 cvx_solve(X_train, X_test, Y_train, Y_test)
+# approximate training RMSE: 17926.230370414116
+# RMSE: 1272853.232496521
+# lambda = 100 fails, "Try recentering the problem data around 0 and rescaling to reduce the dynamic range"
